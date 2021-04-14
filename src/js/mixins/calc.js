59252e00f0064abb8a58e69bf1c14c3b94b9e151
Sławek Trubiłowicz <s.trubilowicz@savicki.pl>
2021-04-14 21:29:50 +0200

    feat: calc

--- src/js/mixins/calc.js
+++ src/js/mixins/calc.js
@@ -1,24 +1,14 @@
-class Calculate {
-    static power = [3.4, 3.74, 4.08, 4.42, 4.76, 5.1, 5.44, 5.78, 6.12, 6.46, 6.8, 7.14, 7.48, 7.82, 8.16, 8.5, 8.84, 9.18, 9.52, 9.86, 15.3, 20.4, 30.6, 40.8, 49.64];
-    static cost = [13900, 16700, 17600, 18550, 19500, 20370, 21300, 22250, 23350, 24350, 25500, 26400, 27600, 28900, 29630, 30700, 31100, 32300, 33500, 35200, 51150, 66100, 96500, 125700, 153000];
+import COSTS from "../config/calculator";
 
-    static calculateCost(pow, panel) {
-        const p = this.power.find((el) => el - pow > 0 && el - pow <= panel);
-        return p ? this.cost[this.power.indexOf(p)] : false;
+export const getCost = (power) => {
+  const powers = Object.keys(COSTS)
+    .map((el) => Number(el))
+    .sort((a, b) => a > b);
+
+  return powers.reduce((acc, current) => {
+    if (power > current) {
+      return COSTS[current];
     }
-
-    static otherInstallation(pow, panel) {
-        const p = this.power.find((el) => el > pow);
-        if (p) {
-            if (this.cost[this.power.indexOf(p)]) return this.cost[this.power.indexOf(p)];
-        } else {
-            return 3*pow*1100;
-        }
-    }
-
-    static getCost(pow, panel) {
-        return this.calculateCost(pow, panel) ? `Szacowany koszt instalacji: ${this.calculateCost(pow, panel)+1000}zł` : `Koszt instalacji nie przekroczy: ${this.otherInstallation(pow, panel)+1000}zł`;
-    }
-}
-
-export default Calculate;
+    return acc;
+  }, COSTS[powers[0]]);
+};
--- src/js/template/Calc.vue
+++ src/js/template/Calc.vue
@@ -1,4 +1,4 @@
-<template lang='pug'>
+<template lang="pug">
     section#calc
         div.calc-header.animation-onshow_left.animation-hide
             | Podaj miesięczny koszt energii:
@@ -27,38 +27,23 @@
                     | Potrzebna moc instalacji:
                     b.color-changer
                         | {{ calculator.neededPower }}kWh
-            div.single-calc
+            div.single-calc(v-for="(mod, index) in calculator.modules" :key="index")
                 p.header
                     | Wyliczenia dla modułów:
                     b
-                        | {{ calculator.module1 }}W
+                        | {{ mod.power }}W
                 p.content
                     p.single-content
                         | Aby uzyskać zalecaną moc potrzebujesz: <br />
                         b.color-changer
-                            | {{ calculator.module1Amount }}szt.
-                        | modułów (<b class='color-changer'>{{ calculator.module1Energy }}kW</b>)
+                            | {{ mod.amount }}szt.
+                        | modułów (<b class='color-changer'>{{ mod.energy }}kW</b>)
                     p.result
                         | Powierzchnia modułów:
                         b.color-changer
-                            | {{ calculator.module1Surface }}m<sup>2</sup>
-            div.single-calc
-                p.header
-                    | Wyliczenia dla modułów:
-                    b
-                        | {{ calculator.module2 }}W
-                p.content
-                    p.single-content
-                        | Aby uzyskać zalecaną moc potrzebujesz: <br />
-                        b.color-changer
-                            | {{ calculator.module2Amount }}szt.
-                        | modułów (<b class='color-changer'>{{ calculator.module2Energy }}kW</b>)
-                    p.result
-                        | Powierzchnia modułów:
-                        b.color-changer
-                            | {{ calculator.module2Surface }}m<sup>2</sup>
+                            | {{ mod.surface }}m<sup>2</sup>
         div.calc-installation.animation-onshow_popup
-            | {{ calculator.installationCost }}
+            | Koszt instalacji nie przekroczy: {{ calculator.installationCost }}zł
             p.warning
                 | Jest to jedynie szacowany koszt instalacji. Cena może się zmienić, zależnie od rodzaju zadaszenia oraz rodzaju dachówki.
         div.calc-header.animation-onshow_left
@@ -67,276 +52,294 @@
                 | {{ calculator.yearlySafe }}
             | PLN / rocznie
         div.calc-info.animation-onshow_left
-            | * - średnia cena prądu z dnia 01.01.2020r (0.55zł/kWh).
+            | * - średnia cena prądu z dnia 01.02.2021r (0.55zł/kWh).
         <ContactMini class='changed-contact'/>
         <Animation />
 </template>
 <script>
-import calc from '../mixins/calc';
-import ContactMini from '../components/Contact-mini.vue';
-import Animation from '../components/Animation.vue';
+import { getCost } from "../mixins/calc";
+import ContactMini from "../components/Contact-mini.vue";
+import Animation from "../components/Animation.vue";
 
 export default {
-    name: 'Calc',
-    components: {
-        ContactMini,
-        Animation
+  name: "Calc",
+  components: {
+    ContactMini,
+    Animation,
+  },
+  data() {
+    return {
+      header: {
+        title: "Kalkulator",
+        subtitle: "",
+        description: "Oblicz koszt instalacji PV",
+        path: window.location.pathname,
+      },
+      calcTimeout: "",
+      calculator: {
+        errorMessage: "",
+        cost: 120,
+        // Energy cost based on http://www.cena-pradu.pl/mapa.html data 2021
+        energyCost: 0.63,
+        energyPower: 0.022,
+        neededSurface: 1.7,
+        yearlyExpense: "",
+        monthlyEnergy: "",
+        yearlyEnergy: "",
+        neededPower: "",
+
+        modules: [
+          {
+            power: 370,
+            amount: "",
+            energy: 0.37,
+            surface: "",
+          },
+          {
+            power: 430,
+            amount: "",
+            energy: 0.43,
+            surface: "",
+          },
+        ],
+        yearlySafe: "",
+        installationCost: "",
+      },
+    };
+  },
+  methods: {
+    componentsColorsChanger(titleColor, subtitleColor, linksColor) {
+      const title = document.querySelector(".title-regular");
+      const titleWrapper = document
+        .querySelector(".title-wrapper")
+        .querySelector("p");
+      const regular = document.querySelectorAll(".regular");
+      const pathWrapper = document
+        .querySelector(".path-wrapper")
+        .querySelector("a");
+      pathWrapper.style.color = linksColor;
+      regular.forEach((el) => (el.style.color = subtitleColor));
+      titleWrapper.style.color = subtitleColor;
+      title.style.color = titleColor;
     },
-    data() {
-        return {
-            header: {
-                title: 'Kalkulator',
-                subtitle: '',
-                description: 'Oblicz koszt instalacji PV',
-                path: window.location.pathname
-            },
-            calc,
-            calcTimeout: '',
-            calculator: {
-                errorMessage: '',
-                cost: 120,
-                // Energy cost based on globenergia.pl data 2020
-                energyCost: 0.50,
-                energyPower: 0.022,
-                neededSurface: 1.7,
-                yearlyExpense: '',
-                monthlyEnergy: '',
-                yearlyEnergy: '',
-                neededPower: '',
-                module1: 325,
-                module1Amount: '',
-                module1Energy: 0.325,
-                module1Surface: '',
-                module2: 340,
-                module2Amount: '',
-                module2Energy: 0.34,
-                module2Surface: '',
-                yearlySafe: '',
-                installationCost: '',
-            }
-        }
+    // Input Validation
+    validateValue(value) {
+      return isNaN(value) ? false : true;
     },
-    methods: {
-        componentsColorsChanger(titleColor, subtitleColor, linksColor) {
-            const title = document.querySelector('.title-regular');
-            const titleWrapper = document.querySelector('.title-wrapper').querySelector('p');
-            const regular = document.querySelectorAll('.regular');
-            const pathWrapper = document.querySelector('.path-wrapper').querySelector('a');
-            pathWrapper.style.color = linksColor;
-            regular.forEach(el => el.style.color = subtitleColor);
-            titleWrapper.style.color = subtitleColor;
-            title.style.color = titleColor;
-        },
-        // Input Validation
-        validateValue(value) {
-            return isNaN(value) ? false : true;
-        },
-        // Calculations and DOM changes
-        calculateValues(value) {
-            const c = this.calculator;
-            c.yearlyExpense = value*12;
-            c.monthlyEnergy = Math.round((value/c.energyCost)*100)/100;
-            c.yearlyEnergy = Math.round((c.monthlyEnergy*12)*100)/100;
-            c.neededPower = Math.round((c.energyPower*value)*100)/100;
-            c.module1Amount = Math.ceil(c.neededPower / c.module1Energy);
-            c.module2Amount = Math.ceil(c.neededPower / c.module2Energy);
-            c.module1Surface = Math.round((c.neededSurface * c.module1Amount*100))/100;
-            c.module2Surface = Math.round((c.neededSurface * c.module2Amount*100))/100;
-            c.yearlySafe = value*12;
-            c.installationCost = this.calc.getCost(c.neededPower, c.module2Energy);
-        },
-        calcService(cost) {
-            const input = document.querySelector('#value');
-            if (!this.validateValue(cost)) {
-                this.calculator.errorMessage = 'Podaj prawidłowy koszt energii (liczba)';
-                input.style.color = '#E84C15';
-                return;
-            }
-            this.calculator.errorMessage = '';
-            input.style.color = '#249fa1';
-            if (cost > 390) {
-                this.calculator.errorMessage = 'Dla tak wysokich rachunków zalecamy wycenę indywidualną';
-                input.style.color = '#E8BE3F';
-            }
-            this.calculateValues(cost);
-        }
+    // Calculations and DOM changes
+    calculateValues(value) {
+      const c = this.calculator;
+      c.yearlyExpense = value * 12;
+      c.monthlyEnergy = (value / c.energyCost).toFixed(0);
+      c.yearlyEnergy = (c.monthlyEnergy * 12).toFixed(0);
+      c.neededPower = (c.energyPower * value).toFixed(0);
+
+      c.modules.forEach((mod) => {
+        mod.amount = Math.ceil(c.neededPower / mod.energy);
+        mod.surface = (c.neededSurface * mod.amount).toFixed(2);
+      });
+
+      c.yearlySafe = value * 12;
+
+      getCost(c.neededPower);
+      c.installationCost = getCost(c.neededPower);
     },
-    mounted() {
-        this.$store.commit('changeTitle', [this.header.title, this.header.subtitle, this.header.description, this.header.path]);
-        this.calcTimeout = setTimeout(() => {
-            this.componentsColorsChanger('#fff', '#249fa1', '#fff');
-            this.calculateValues(this.calculator.cost);
-        }, 100);
-        window.scrollTo(0,0);
+    calcService(cost) {
+      const input = document.querySelector("#value");
+      if (!this.validateValue(cost)) {
+        this.calculator.errorMessage =
+          "Podaj prawidłowy koszt energii (liczba)";
+        input.style.color = "#E84C15";
+        return;
+      }
+      this.calculator.errorMessage = "";
+      input.style.color = "#249fa1";
+      if (cost > 390) {
+        this.calculator.errorMessage =
+          "Dla tak wysokich rachunków zalecamy wycenę indywidualną";
+        input.style.color = "#E8BE3F";
+      }
+      this.calculateValues(cost);
     },
-    destroyed() {
-        clearTimeout(this.calcTimeout);
-        this.$store.state.header.renderPath = false;
-        this.componentsColorsChanger('#000', '#555', '#000');
-    }
-}
+  },
+  mounted() {
+    this.$store.commit("changeTitle", [
+      this.header.title,
+      this.header.subtitle,
+      this.header.description,
+      this.header.path,
+    ]);
+    this.calcTimeout = setTimeout(() => {
+      this.componentsColorsChanger("#fff", "#249fa1", "#fff");
+      this.calculateValues(this.calculator.cost);
+    }, 100);
+    window.scrollTo(0, 0);
+  },
+  destroyed() {
+    clearTimeout(this.calcTimeout);
+    this.$store.state.header.renderPath = false;
+    this.componentsColorsChanger("#000", "#555", "#000");
+  },
+};
 </script>
 <style lang="scss">
-    
-    $darkCyan: #186a6b;
-    $lightCyan: #249fa1;
+#calc {
+  width: 100%;
+  height: auto;
 
-    #calc {
-        width: 100%;
-        height: auto;
+  background-color: #005b6b;
+  color: #fff;
+  padding: 250px 5px 100px 5px;
+  position: absolute;
+  top: 0;
+  left: 0;
 
+  .color-changer {
+    color: $color-primary;
+    margin: 0 5px;
+  }
+
+  .calc-installation {
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+
+    width: 100%;
+    font-size: 1.1em;
+
+    .warning {
+      margin: 15px 0 30px 0;
+      color: goldenrod;
+      font-size: 0.6em;
+    }
+  }
+
+  .calc-header {
+    width: 100%;
+    margin: 30px 0;
+    text-align: center;
+
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.2em;
+    position: relative;
+
+    .error-handler {
+      color: #e8be3f;
+      font-size: 0.8em;
+      width: 100%;
+      text-align: center;
+      position: absolute;
+      bottom: -20px;
+    }
+
+    input {
+      margin: 20px 0;
+      background: none;
+      border: none;
+      border-bottom: 2px solid $color-primary;
+      width: 150px;
+      font-size: 1.8em;
+      color: $color-primary;
+      text-align: center;
+      font-weight: 700;
+    }
+
+    b {
+      font-size: 1.2em;
+    }
+  }
+
+  .calc-wrapper {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-gap: 80px 0;
+    margin: 50px 0;
+    align-items: center;
+    justify-content: center;
+
+    text-align: center;
+    font-size: 1.2em;
+
+    .single-calc {
+      display: flex;
+      flex-flow: column;
+      align-items: center;
+      justify-items: flex-start;
+      font-size: 0.7em;
+      height: 100%;
+
+      .single-content {
+        line-height: 1.5;
+      }
+
+      .result {
+        margin-top: 20px;
+      }
+
+      b {
+        margin: 0 5px;
+      }
+
+      .header {
+        font-size: 1.2em;
+        margin-bottom: 20px;
+        text-transform: uppercase;
+
+        b {
+          color: $color-primary;
+          font-weight: 700;
+        }
+      }
+    }
+  }
+
+  .changed-contact {
+    margin: 150px 0;
+
+    a {
+      button {
+        border: 1px solid $color-primary;
         background-color: #005b6b;
-        color: #fff;
-        padding: 250px 5px 100px 5px;
-        position: absolute;
-        top: 0;
-        left: 0;
-    
-        .color-changer {
-            color: $lightCyan;
-            margin: 0 5px;
+
+        &:hover {
+          background-color: $color-primary;
+          border-radius: 10px;
         }
-
-        .calc-installation {
-            display: flex;
-            flex-flow: column;
-            align-items: center;
-            justify-content: center;
-            text-align: center;
-
-            width: 100%;
-            font-size: 1.1em;
-
-            .warning {
-                margin: 15px 0 30px 0;
-                color: goldenrod;
-                font-size: .6em;
-            }
-        }
-
-        .calc-header {
-            width: 100%;
-            margin: 30px 0;
-            text-align: center;
-
-            display: flex;
-            flex-flow: column;
-            align-items: center;
-            justify-content: center;
-            font-size: 1.2em;
-            position: relative;
-
-            .error-handler {
-                color :#E8BE3F;
-                font-size: .8em;
-                width: 100%;
-                text-align: center;
-                position: absolute;
-                bottom: -20px;
-            }
-
-            input {
-                margin: 20px 0;
-                background: none;
-                border: none;
-                border-bottom: 2px solid $lightCyan;
-                width: 150px;
-                font-size: 1.8em;
-                color: $lightCyan;
-                text-align: center;
-                font-weight: 700;
-            }
-
-            b {
-                font-size: 1.2em;
-            }
-        }
-
-        .calc-wrapper {
-            display: grid;
-            grid-template-columns: 1fr;
-            grid-gap: 80px 0;
-            margin: 50px 0;
-            align-items: center;
-            justify-content: center;
-
-            text-align: center;
-            font-size: 1.2em;
-
-            .single-calc {
-                display: flex;
-                flex-flow: column;
-                align-items: center;
-                justify-items: flex-start;
-                font-size: .7em;
-                height: 100%;
-
-                .single-content {
-                    line-height: 1.5;
-                }
-
-                .result {
-                    margin-top: 20px;
-                }
-
-                b {
-                    margin: 0 5px;
-                }
-
-                .header {
-                    font-size: 1.2em;
-                    margin-bottom: 20px;
-                    text-transform: uppercase;
-
-                    b {
-                        color: $lightCyan;
-                        font-weight: 700;
-                    }
-                }
-            }
-        }
-
-        .changed-contact {
-            margin: 150px 0;
-
-            a {
-                button {
-                    border: 1px solid $lightCyan;
-                    background-color: #005b6b;
-
-                    &:hover {
-                        background-color: $lightCyan;
-                        border-radius: 10px;
-                    }
-                }
-            }
-        }
-
-        .calc-info {
-            width: 100%;
-            font-size: .7em;
-            text-align: center;
-        }
+      }
     }
+  }
 
-@media (min-width: 768px) {
-
-    #calc {
-        .calc-header {
-            flex-flow: row;
-        }
-
-        .calc-wrapper {
-            margin: 100px 0;
-            grid-template-columns: repeat(3, 1fr);
-            font-size: .8em;
-
-            .single-content {
-                line-height: 2;
-            }
-        }
-    }
-
+  .calc-info {
+    width: 100%;
+    font-size: 0.7em;
+    text-align: center;
+  }
 }
 
+@media (min-width: 768px) {
+  #calc {
+    .calc-header {
+      flex-flow: row;
+    }
+
+    .calc-wrapper {
+      margin: 100px 0;
+      grid-template-columns: repeat(3, 1fr);
+      font-size: 0.8em;
+
+      .single-content {
+        line-height: 2;
+      }
+    }
+
+    .single-calc {
+      font-size: 1em !important;
+    }
+  }
+}
 </style>
