056e669413bc9b2d67f0cfb65b30894aa0d2c1e6
Sławek Trubiłowicz <s.trubilowicz@savicki.pl>
2021-04-15 21:31:06 +0200

    fix: gallery

--- src/js/components/about/Projects.vue
+++ src/js/components/about/Projects.vue
@@ -56,12 +56,12 @@ export default {
 
   grid-template-columns: repeat(4, 1fr);
 
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
   @media (max-width: 1024px) {
     grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
   }
 
   & > .photo-box {
