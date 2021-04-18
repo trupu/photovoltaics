a00b8bbf5ff02733426720b14f44bd5f32720422
Sławek Trubiłowicz <s.trubilowicz@savicki.pl>
2021-04-18 15:55:51 +0200

    feat: meta tags

--- src/js/template/Contact.vue
+++ src/js/template/Contact.vue
@@ -1,59 +1,62 @@
 <template lang="pug">
     section#contact
         <contactData />
-        <contactForm />
+        //- <contactForm />
 </template>
 <script>
-import contactData from '../components/contact/contactData.vue';
-import contactForm from '../components/contact/contactForm.vue';
+import contactData from "../components/contact/contactData.vue";
+import contactForm from "../components/contact/contactForm.vue";
 
 export default {
-    name: 'Contact',
-    components: {
-        contactData,
-        contactForm
-    },
-    data() {
-        return {
-            header: {
-                title: 'Kontakt',
-                subtitle: '',
-                description: 'Jak można się z nami skontaktować?',
-                path: window.location.pathname
-            }
-        }
-    },
-    mounted() {
-        this.$store.commit('changeTitle', [this.header.title, this.header.subtitle, this.header.description, this.header.path]);
-        window.scrollTo(0,0);
-    },
-    destroyed() {
-        this.$store.state.header.renderPath = false;
-    }
-}
+  name: "Contact",
+  components: {
+    contactData,
+    contactForm,
+  },
+  data() {
+    return {
+      header: {
+        title: "Kontakt",
+        subtitle: "",
+        description: "Jak można się z nami skontaktować?",
+        path: window.location.pathname,
+      },
+    };
+  },
+  mounted() {
+    this.$store.commit("changeTitle", [
+      this.header.title,
+      this.header.subtitle,
+      this.header.description,
+      this.header.path,
+    ]);
+    window.scrollTo(0, 0);
+  },
+  destroyed() {
+    this.$store.state.header.renderPath = false;
+  },
+};
 </script>
 <style lang="scss">
-    
-    $darkCyan: #186a6b;
-    $lightCyan: #249fa1;
+$darkCyan: #186a6b;
+$lightCyan: #249fa1;
 
-    #contact {
-        display: flex;
-        flex-flow: column;
-        align-items: center;
-        justify-content: center;
+#contact {
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
 
-        width: 80%;
-        margin: 0 0 100px 0;
-    }
-
-@media (min-width: 768px) {
-    #contact {
-        flex-flow: row;
-        align-items: flex-start;
-
-        margin: 50px 0 100px 0;
-    }
+  width: 80%;
+  margin: 0 0 100px 0;
 }
 
+@media (min-width: 768px) {
+  #contact {
+    flex-flow: row;
+    align-items: flex-start;
+
+    margin: 50px 0 100px 0;
+  }
+}
 </style>
