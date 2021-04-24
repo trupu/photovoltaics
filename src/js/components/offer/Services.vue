3e8faf2552169ea811887391f7e0ffde18cd21b8
Sławek Trubiłowicz <s.trubilowicz@savicki.pl>
2021-04-24 14:39:52 +0200

    feat: add info

--- src/js/components/offer/Services.vue
+++ src/js/components/offer/Services.vue
@@ -7,75 +7,77 @@
                 h3.title
                     | Czym się zajmujemy?
                 p.content
-                    | Zajmujemy się profesjonalnym montażem paneli fotowoltaicznych na terenie gospodarstwa domowego. Panele PV pokryte są szkłem hartowanym, gwarantującym wysoką odporność na uszkodzenia spowodowane warunkami atmosferycznymi. <br/><br/> W naszej ofercie znajduje się instalacja PV zarówno na dachu, jak i na gruncie.
+                    | Zajmujemy się profesjonalnym montażem paneli fotowoltaicznych na terenie gospodarstwa domowego. Panele PV pokryte są szkłem hartowanym, gwarantującym wysoką odporność na uszkodzenia spowodowane warunkami atmosferycznymi. <br/><br/> 
+                    | W naszej ofercie znajdują się: <br /><br />
+                    | - profesjonalna instalacja <b>paneli fotowoltaicznych</b> (na dachu / gruncie), <br />
+                    | - montaż <b>pomp ciepła</b>, <br />
+                    | - kompleksowy montaż nowoczesnych i ekonomicznych <b>systemów ogrzewania podczerwienią</b>, <br />
+                    | - montaż <b>mat grzewczych</b>
 </template>
 <script>
-import Animation from '../Animation.vue';
+import Animation from "../Animation.vue";
 
 export default {
-    name: 'Services',
-    components: {
-        Animation
-    }
-}
+  name: "Services",
+  components: {
+    Animation,
+  },
+};
 </script>
 <style lang="scss">
-    #services {
-        .services-wrapper {
-            display: flex;
-            flex-flow: column-reverse;
-            align-items: center;
-            justify-content: center;
+#services {
+  .services-wrapper {
+    display: flex;
+    flex-flow: column-reverse;
+    align-items: center;
+    justify-content: center;
 
-            width: 100%;
-            height: auto;
+    width: 100%;
+    height: auto;
 
-            .services-img {
-                margin: 20px 0;
-                width: 100%;
-                height: 300px;
+    .services-img {
+      margin: 20px 0;
+      width: 100%;
+      height: 300px;
 
-                img {
-                    object-fit: cover;
-                    height: 300px;
-                    width: 100%;
-                }
-            }
-
-            .services-content {
-
-                width: 100%;
-                padding: 0 5px;
-
-                .title {
-                    font-size: 1.2em;
-                    font-weight: 400;
-                    margin: 0 0 20px 0;
-                }
-
-                .content {
-                    font-size: 1em;
-                }
-            }
-        }
+      img {
+        object-fit: cover;
+        height: 300px;
+        width: 100%;
+      }
     }
 
-    @media (min-width: 768px) {
+    .services-content {
+      width: 100%;
+      padding: 0 5px;
 
-        #services {
-            .services-wrapper {
-                flex-flow: row;
-                justify-items: flex-start;
+      .title {
+        font-size: 1.2em;
+        font-weight: 400;
+        margin: 0 0 20px 0;
+      }
 
-                .services-content {
-                    height: 100%;
-                }
-
-                .services-img {
-                    margin: 0 20px;
-                }
-            }
-        }
-
+      .content {
+        font-size: 1em;
+      }
     }
+  }
+}
+
+@media (min-width: 768px) {
+  #services {
+    .services-wrapper {
+      flex-flow: row;
+      justify-items: flex-start;
+
+      .services-content {
+        height: 100%;
+      }
+
+      .services-img {
+        margin: 0 20px;
+      }
+    }
+  }
+}
 </style>
